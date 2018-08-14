3e172c8c91f9b450598ba8d46e1b2e87956ac021
monicaadvenia <monica_advenia@yahoo.com>
2018-08-14 07:44:40 +0700

    Fix config

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(passport.initialize());
 
 const mysql = require('mysql');
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'charon',
-    database: 'coba_movietime_01'
+    host: 'mcldisu5ppkm29wf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'mfqe7pjw1yqnku1f',
+    password: 'qhtezq8xhki8p2v3',
+    database: 'ffgf4yfdlp7nly40'
 });
 db.connect();
 
